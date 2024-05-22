6e2da027ec4cd029648b07c4d574676dea3c89b3
Dusan Klinec <dklinec@purestorage.com>
2024-05-22 18:40:50 +0200

    [TECH] update purelogin formula to v1.3.24

--- Formula/purelogin.rb
+++ Formula/purelogin.rb
@@ -2,9 +2,9 @@ class Purelogin < Formula
   desc "Purelogin and Keymaster client binaries"
   homepage "https://github.com/pure-product-security/purelogin2"
 
-  url "https://artifactory.pstg-inf.net/artifactory/pstg-maven-official/com/purestorage/purelogin/1.3.23/purelogin-1.3.23-osx.tgz"
-  version "1.3.23"
-  sha256 "7e479fa36d7b73141d5e3d48f8e5ea04ddd17ae7815b775125696a590dc116c1"
+  url "https://artifactory.pstg-inf.net/artifactory/pstg-maven-official/com/purestorage/purelogin/1.3.24/purelogin-1.3.24-osx.tgz"
+  version "1.3.24"
+  sha256 "0ffd056fce108f2203671b940984aa13afb4a04b8d5e82c13a48f1ff9e8cbe34"
 
   def install
     bin.install "purelogin", "keymaster"
