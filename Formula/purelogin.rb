a8cb609598b4e60d380b03d44852d23793214c0c
Dusan Klinec <dklinec@purestorage.com>
2024-05-22 17:22:26 +0200

    [TECH] update purelogin formula to v1.3.23 - maven repo

--- Formula/purelogin.rb
+++ Formula/purelogin.rb
@@ -2,7 +2,7 @@ class Purelogin < Formula
   desc "Purelogin and Keymaster client binaries"
   homepage "https://github.com/pure-product-security/purelogin2"
 
-  url "https://artifactory.pstg-inf.net/artifactory/pstg-maven-official-local/com/purestorage/purelogin/1.3.23/purelogin-1.3.23-osx.tgz"
+  url "https://artifactory.pstg-inf.net/artifactory/pstg-maven-official/com/purestorage/purelogin/1.3.23/purelogin-1.3.23-osx.tgz"
   version "1.3.23"
   sha256 "7e479fa36d7b73141d5e3d48f8e5ea04ddd17ae7815b775125696a590dc116c1"
 
